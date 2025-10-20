59747da90aaafd9a2709cb08f02a21f4952542ca
James Mills <jcmills.developer@gmail.com>
2025-10-20 13:03:35 +1100

    Beginning of mesh system

--- Engine/Rendering/StaticMesh.cs
+++ Engine/Rendering/StaticMesh.cs
@@ -1,7 +1,30 @@
-﻿namespace HoleIO.Engine.Rendering
+﻿using System.Drawing;
+using System.Numerics;
+using Assimp.Unmanaged;
+
+namespace HoleIO.Engine.Rendering
 {
 	public class StaticMesh
 	{
-		
+		public struct Vertex
+		{
+			public Vector4 position;
+			public Vector4 normal;
+			public Vector4 tangent;
+			public Vector4 biTangent;
+
+			public Vector2[] uvs;
+			public Color[] colors;
+		}
+
+		private uint triCount;
+		private uint vao;
+		private uint vbo;
+		private uint ibo;
+
+		public void LoadFromAssimp(string filename, bool flipTextureV = false)
+		{
+			
+		}
 	}
 }
